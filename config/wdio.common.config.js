ef1889c391219da6e849767c71cabad74134fd86
Tarun <saitarun.adavala.partner@decathlon.com>
2022-02-21 16:22:50 +0530

    allure feature

--- config/wdio.common.config.js
+++ config/wdio.common.config.js
@@ -1,3 +1,5 @@
+const allure = require("allure-commandline");
+
 exports.config = {
     logLevel: "info",
     framework: "mocha",
@@ -6,6 +8,7 @@ exports.config = {
         require: ["@babel/register"],
         timeout: 60000,
     },
+    waitforTimeout: 10000,
     sync: true,
     services: [
         [
@@ -16,6 +19,37 @@ exports.config = {
             },
         ],
     ],
+    reporters: [
+        [
+            "allure",
+            {
+                outputDir: "allure-results",
+                disableWebdriverStepsReporting: true,
+                disableWebdriverScreenshotsReporting: true,
+            },
+        ],
+    ],
+
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
 
     after: function () {
         driver.removeApp("com.evamall.evacustomer");
